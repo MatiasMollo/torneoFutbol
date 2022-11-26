09c5ef711ba1eaf560162645d93faa9964e9e07c
Matias Mollo <mollo.matias03@gmail.com>
2022-11-25 21:03:30 -0300

    Responsive fechas.php

--- fechas.php
+++ fechas.php
@@ -34,6 +34,8 @@ $sql = "SELECT * FROM jugadores WHERE goles>=1 ORDER BY goles DESC LIMIT 10";
 $consulta = $conn->query($sql);
 $goleadores = mysqli_fetch_all($consulta);
 
+//! Falta usar el sistema de filas y columnas en el acordion y en los datos de equipos (tarjetas)
+
 ?>
 
 <!DOCTYPE html>
@@ -44,10 +46,11 @@ $goleadores = mysqli_fetch_all($consulta);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/fechas.css"/>
     <title>Torneo</title>
 </head>
 <body class="bg-dark">
-    <div class="accordion accordion-flush mx-auto w-50 rounded mt-5" id="accordionFlushExample">
+    <div class="accordion accordion-flush mx-auto container-sm rounded mt-5" id="accordionFlushExample">
         <div class="accordion-item">
           <h2 class="accordion-header" id="flush-headingOne">
             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
@@ -56,27 +59,27 @@ $goleadores = mysqli_fetch_all($consulta);
           </h2>
           <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
             <div class="accordion-body">
-               <div class="teamContainer d-flex h3 position-relative">
+               <div class="teamContainer position-relative h3 container">
                     <a href="verEquipo.php?id=<?= $equipos[0][0] ?>"><?= $equipos[0][1] ?></a>
-                    <h3 class="ms-4 me-4">VS</h3>
+                    <h3>VS</h3>
                     <a href="verEquipo.php?id=<?= $equipos[1][0] ?>"><?= $equipos[1][1] ?></a>
                     <div class="mx-auto me-1 float-end">
                         <a href="jugarFecha.php?id=1&E1=<?= $equipos[0][0] ?>&E2=<?= $equipos[1][0] ?>" 
                             class="btn btn-primary <?php echo $idPartido != 1 ? "disabled" : "" ?>">Jugar</a>
                     </div>
                 </div>
-               <div class="teamContainer d-flex justify-content-center h3 position-relative">
+               <div class="teamContainer position-relative h3 container">
                     <a href="verEquipo.php?id=<?= $equipos[2][0] ?>"><?= $equipos[2][1] ?></a>
-                    <h3 class="ms-4 me-4">VS</h3>
+                    <h3>VS</h3>
                     <a href="verEquipo.php?id=<?= $equipos[3][0] ?>"><?= $equipos[3][1] ?></a>
                     <div class="mx-auto me-1 float-end">
                         <a href="jugarFecha.php?id=2&E1=<?= $equipos[2][0] ?>&E2=<?= $equipos[3][0] ?>"
                             class="btn btn-primary <?php echo $idPartido != 2 ? "disabled" : "" ?>">Jugar</a>
                     </div>
                </div>
-               <div class="teamContainer d-flex justify-content-center h3 position-relative">
+               <div class="teamContainer position-relative h3 container">
                     <a href="verEquipo.php?id=<?= $equipos[4][0] ?>"><?= $equipos[4][1] ?></a>
-                    <h3 class="ms-4 me-4">VS</h3>
+                    <h3>VS</h3>
                     <a href="verEquipo.php?id=<?= $equipos[5][0] ?>"><?= $equipos[5][1] ?></a>
                     <div class="mx-auto me-1 float-end">
                         <a href="jugarFecha.php?id=3&E1=<?= $equipos[4][0] ?>&E2=<?= $equipos[5][0] ?>" 
@@ -94,27 +97,27 @@ $goleadores = mysqli_fetch_all($consulta);
           </h2>
           <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
             <div class="accordion-body">
-               <div class="teamContainer d-flex h3 position-relative">
+               <div class="teamContainer position-relative h3 container">
                     <a href="verEquipo.php?id=<?= $equipos[0][0] ?>"><?= $equipos[0][1] ?></a>
-                    <h3 class="ms-4 me-4">VS</h3>
+                    <h3>VS</h3>
                     <a href="verEquipo.php?id=<?= $equipos[5][0] ?>"><?= $equipos[5][1] ?></a>
                     <div class="mx-auto me-1 float-end">
                         <a href="jugarFecha.php?id=4&E1=<?= $equipos[0][0] ?>&E2=<?= $equipos[5][0] ?>"
                             class="btn btn-primary <?php echo $idPartido != 4 ? "disabled" : "" ?>">Jugar</a>
                     </div>
                 </div>
-               <div class="teamContainer d-flex h3 position-relative">
+               <div class="teamContainer position-relative h3 container">
                     <a href="verEquipo.php?id=<?= $equipos[1][0] ?>"><?= $equipos[1][1] ?></a>
-                    <h3 class="ms-4 me-4">VS</h3>
+                    <h3>VS</h3>
                     <a href="verEquipo.php?id=<?= $equipos[2][0] ?>"><?= $equipos[2][1] ?></a>
                     <div class="mx-auto me-1 float-end">
                         <a href="jugarFecha.php?id=5&E1=<?= $equipos[1][0] ?>&E2=<?= $equipos[2][0] ?>" 
                             class="btn btn-primary <?php echo $idPartido != 5 ? "disabled" : "" ?>">Jugar</a>
                     </div>
                </div>
-               <div class="teamContainer d-flex h3 position-relative">
+               <div class="teamContainer position-relative h3 container">
                     <a href="verEquipo.php?id=<?= $equipos[3][0] ?>"><?= $equipos[3][1] ?></a>
-                    <h3 class="ms-4 me-4">VS</h3>
+                    <h3>VS</h3>
                     <a href="verEquipo.php?id=<?= $equipos[4][0] ?>"><?= $equipos[4][1] ?></a>
                     <div class="mx-auto me-1 float-end">
                         <a href="jugarFecha.php?id=6&E1=<?= $equipos[3][0] ?>&E2=<?= $equipos[4][0] ?>" 
@@ -132,27 +135,27 @@ $goleadores = mysqli_fetch_all($consulta);
           </h2>
           <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
           <div class="accordion-body">
-               <div class="teamContainer d-flex h3 position-relative">
+               <div class="teamContainer position-relative h3 container">
                     <a href="verEquipo.php?id=<?= $equipos[0][0] ?>"><?= $equipos[0][1] ?></a>
-                    <h3 class="ms-4 me-4">VS</h3>
+                    <h3>VS</h3>
                     <a href="verEquipo.php?id=<?= $equipos[2][0] ?>"><?= $equipos[2][1] ?></a>
                     <div class="mx-auto me-1 float-end">
                         <a href="jugarFecha.php?id=7&E1=<?= $equipos[0][0] ?>&E2=<?= $equipos[2][0] ?>" 
                             class="btn btn-primary <?php echo $idPartido != 7 ? "disabled" : "" ?>">Jugar</a>
                     </div>
                 </div>
-               <div class="teamContainer d-flex h3 position-relative">
+               <div class="teamContainer position-relative h3 container">
                     <a href="verEquipo.php?id=<?= $equipos[1][0] ?>"><?= $equipos[1][1] ?></a>
-                    <h3 class="ms-4 me-4">VS</h3>
+                    <h3>VS</h3>
                     <a href="verEquipo.php?id=<?= $equipos[4][0] ?>"><?= $equipos[4][1] ?></a>
                     <div class="mx-auto me-1 float-end">
                         <a href="jugarFecha.php?id=8&E1=<?= $equipos[1][0] ?>&E2=<?= $equipos[4][0] ?>" 
                             class="btn btn-primary <?php echo $idPartido != 8 ? "disabled" : "" ?>">Jugar</a>
                     </div>
                </div>
-               <div class="teamContainer d-flex h3 position-relative">
+               <div class="teamContainer position-relative h3 container">
                     <a href="verEquipo.php?id=<?= $equipos[3][0] ?>"><?= $equipos[3][1] ?></a>
-                    <h3 class="ms-4 me-4">VS</h3>
+                    <h3>VS</h3>
                     <a href="verEquipo.php?id=<?= $equipos[5][0] ?>"><?= $equipos[5][1] ?></a>
                     <div class="mx-auto me-1 float-end">
                         <a href="jugarFecha.php?id=9&E1=<?= $equipos[3][0] ?>&E2=<?= $equipos[5][0] ?>" 
@@ -171,27 +174,27 @@ $goleadores = mysqli_fetch_all($consulta);
           </h2>
           <div id="flush-collapseFour" class="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
           <div class="accordion-body">
-               <div class="teamContainer d-flex h3 position-relative">
+               <div class="teamContainer position-relative h3 container">
                     <a href="verEquipo.php?id=<?= $equipos[0][0] ?>"><?= $equipos[0][1] ?></a>
-                    <h3 class="ms-4 me-4">VS</h3>
+                    <h3>VS</h3>
                     <a href="verEquipo.php?id=<?= $equipos[3][0] ?>"><?= $equipos[3][1] ?></a>
                     <div class="mx-auto me-1 float-end">
                         <a href="jugarFecha.php?id=10&E1=<?= $equipos[0][0] ?>&E2=<?= $equipos[3][0] ?>" 
                             class="btn btn-primary <?php echo $idPartido != 10 ? "disabled" : "" ?>">Jugar</a>
                     </div>
                 </div>
-               <div class="teamContainer d-flex h3 position-relative">
+               <div class="teamContainer position-relative h3 container">
                     <a href="verEquipo.php?id=<?= $equipos[1][0] ?>"><?= $equipos[1][1] ?></a>
-                    <h3 class="ms-4 me-4">VS</h3>
+                    <h3>VS</h3>
                     <a href="verEquipo.php?id=<?= $equipos[5][0] ?>"><?= $equipos[5][1] ?></a>
                     <div class="mx-auto me-1 float-end">
                         <a href="jugarFecha.php?id=11&E1=<?= $equipos[1][0] ?>&E2=<?= $equipos[5][0] ?>" 
                             class="btn btn-primary <?php echo $idPartido != 11 ? "disabled" : "" ?>">Jugar</a>
                     </div>
                </div>
-               <div class="teamContainer d-flex h3 position-relative">
+               <div class="teamContainer position-relative h3 container">
                     <a href="verEquipo.php?id=<?= $equipos[2][0] ?>"><?= $equipos[2][1] ?></a>
-                    <h3 class="ms-4 me-4">VS</h3>
+                    <h3>VS</h3>
                     <a href="verEquipo.php?id=<?= $equipos[4][0] ?>"><?= $equipos[4][1] ?></a>
                     <div class="mx-auto me-1 float-end">
                         <a href="jugarFecha.php?id=12&E1=<?= $equipos[2][0] ?>&E2=<?= $equipos[4][0] ?>" 
@@ -210,27 +213,27 @@ $goleadores = mysqli_fetch_all($consulta);
           </h2>
           <div id="flush-collapseFive" class="accordion-collapse collapse" aria-labelledby="flush-headingFive" data-bs-parent="#accordionFlushExample">
             <div class="accordion-body">
-                 <div class="teamContainer d-flex h3 position-relative">
+                 <div class="teamContainer position-relative h3 container">
                       <a href="verEquipo.php?id=<?= $equipos[1][0] ?>"><?= $equipos[1][1] ?></a>
-                      <h3 class="ms-4 me-4">VS</h3>
+                      <h3>VS</h3>
                       <a href="verEquipo.php?id=<?= $equipos[3][0] ?>"><?= $equipos[3][1] ?></a>
                       <div class="mx-auto me-1 float-end">
                           <a href="jugarFecha.php?id=13&E1=<?= $equipos[1][0] ?>&E2=<?= $equipos[3][0] ?>" 
                               class="btn btn-primary <?php echo $idPartido != 13 ? "disabled" : "" ?>">Jugar</a>
                       </div>
                   </div>
-                 <div class="teamContainer d-flex h3 position-relative">
+                 <div class="teamContainer position-relative h3 container">
                       <a href="verEquipo.php?id=<?= $equipos[0][0] ?>"><?= $equipos[0][1] ?></a>
-                      <h3 class="ms-4 me-4">VS</h3>
+                      <h3>VS</h3>
                       <a href="verEquipo.php?id=<?= $equipos[4][0] ?>"><?= $equipos[4][1] ?></a>
                       <div class="mx-auto me-1 float-end">
                           <a href="jugarFecha.php?id=14&E1=<?= $equipos[0][0] ?>&E2=<?= $equipos[4][0] ?>" 
                               class="btn btn-primary <?php echo $idPartido != 14 ? "disabled" : "" ?>">Jugar</a>
                       </div>
                  </div>
-                 <div class="teamContainer d-flex h3 position-relative">
+                 <div class="teamContainer position-relative h3 container">
                       <a href="verEquipo.php?id=<?= $equipos[2][0] ?>"><?= $equipos[2][1] ?></a>
-                      <h3 class="ms-4 me-4">VS</h3>
+                      <h3>VS</h3>
                       <a href="verEquipo.php?id=<?= $equipos[5][0] ?>"><?= $equipos[5][1] ?></a>
                       <div class="mx-auto me-1 float-end">
                           <a href="jugarFecha.php?id=15&E1=<?= $equipos[2][0] ?>&E2=<?= $equipos[5][0] ?>" 
@@ -240,9 +243,9 @@ $goleadores = mysqli_fetch_all($consulta);
               </div>
             </div>
         </div>
-        <div class="cardContainer d-flex row justify-content-center mt-2">
+        <div class="cardContainer container row justify-content-around mt-2 mx-auto">
           <?php foreach($equipos as $equipo): ?>
-            <div class="card mx-auto mt-3" style="width: 30%;">
+            <div class="card mx-auto mt-3 col-12 d-flex">
               <div class="card-body">
                 <h5 class="card-title"><?= $equipo[1] ?></h5>
                 <h6 class="card-subtitle mb-2 text-success"><?= $equipo[8] ?> Puntos en el torneo</h6>
@@ -252,7 +255,7 @@ $goleadores = mysqli_fetch_all($consulta);
             </div>
           <?php endforeach; ?>
         </div>
-        <div class="text-light p-3 mt-2">
+        <div class="text-light p-3 mt-2 container">
           <h3>Top 3 equipos</h3>
           <table class="table table-dark table-striped">
             <thead>
@@ -273,49 +276,51 @@ $goleadores = mysqli_fetch_all($consulta);
             </tbody>
         </table>
         <?php if($empate || true): ?>
-          <div class="container card text-dark p-2">
-            <h3 class="ms-4 mt-2 text-danger text-decoration-underline">Empate</h3>
-            <div class="d-flex position-relative h4 ms-4">
-              <a href="verEquipo.php?id=<?= $ganadores[0][0] ?>" class="me-3"><?= $ganadores[0][1] ?></a>
-              VS
-              <a href="verEquipo.php?id=<?= $ganadores[1][0] ?>" class="ms-3"><?= $ganadores[1][1] ?></a>
-              <div class="d-flex mx-auto float-end me-3">
+          <div class="container card text-dark p-0 row d-flex text-center mx-auto div-empate col-sm-6">
+            <h3 class="mt-2 text-danger col-12">Empate</h3>
+            <div class="d-flex position-relative row mx-auto">
+              <a href="verEquipo.php?id=<?= $ganadores[0][0] ?>" class="col-12 p-0"><?= $ganadores[0][1] ?></a>
+              <p class="mx-auto mt-1 mb-1 p-0">VS</p>
+              <a href="verEquipo.php?id=<?= $ganadores[1][0] ?>" class="col-12 p-0"><?= $ganadores[1][1] ?></a>
+              <div class="d-flex mx-auto col-12 div-btn-empate">
                 <a href="jugarFecha.php?id=16&E1=<?= $ganadores[0][0] ?>&E2=<?= $ganadores[1][0] ?>"
-                class="btn btn-primary">Desempatar</a>
+                class="btn btn-primary d-flex mx-auto">Desempatar</a>
               </div>
             </div>
           </div>
         <?php endif ?>
           <h3 class="mt-3 mb-3">Top 10 goleadores</h3>
-          <table class="table table-striped table-dark">
-            <thead>
-              <tr class="table-primary">
-                <th scope="col">Puesto</th>
-                <th scope="col">Nombre</th>
-                <th scope="col">Goles</th>
-                <th scope="col">Puesto</th>
-                <th scope="col">Equipo</th>
-                <?php //! Si no hay ganador y empatan se tiene que definir otro partido!! ?>
-              </tr>
-            </thead>
-            <tbody>
-              <?php foreach($goleadores as $i => $jugador): ?>
-              <tr>
-                <th scope="row"><?= $i+1 ?>°</th>
-                <td scope="row"><?= $jugador[1] . " " . $jugador[2] ?></td>
-                <td scope="row"><?= $jugador[9] ?></td>
-                <td scope="row"><?= ucwords($jugador[5]) ?></td>
-                <?php foreach($equipos as $x => $equipo):
-                  if($equipo[0] == $jugador[8]): ?>
-                  <td scope="row"><?= $equipo[1] ?></td>
-                <?php
-                  endif;
-                  endforeach; 
-                ?>
-              </tr>
-              <?php endforeach; ?>
-            </tbody>
-        </table>
+          <div class="d-flex container p-0 div-tabla-jugadores">
+            <table class="table table-striped table-dark tabla-jugadores">
+              <thead>
+                <tr class="table-primary">
+                  <th scope="col">Puesto</th>
+                  <th scope="col">Nombre</th>
+                  <th scope="col">Goles</th>
+                  <th scope="col">Puesto</th>
+                  <th scope="col">Equipo</th>
+                  <?php //! Si no hay ganador y empatan se tiene que definir otro partido!! ?>
+                </tr>
+              </thead>
+              <tbody>
+                <?php foreach($goleadores as $i => $jugador): ?>
+                <tr>
+                  <th scope="row"><?= $i+1 ?>°</th>
+                  <td scope="row"><?= $jugador[1] . " " . $jugador[2] ?></td>
+                  <td scope="row"><?= $jugador[9] ?></td>
+                  <td scope="row"><?= ucwords($jugador[5]) ?></td>
+                  <?php foreach($equipos as $x => $equipo):
+                    if($equipo[0] == $jugador[8]): ?>
+                    <td scope="row"><?= $equipo[1] ?></td>
+                  <?php
+                    endif;
+                    endforeach; 
+                  ?>
+                </tr>
+                <?php endforeach; ?>
+              </tbody>
+            </table>
+          </div>
         </div>
         <a href="tools/cerrarSesion.php" class="btn btn-light mx-auto d-flex float-end me-2 mt-2 mb-5">Cerrar Sesion</a>
     </div>
